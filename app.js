f23bb8fb8db2e90dfb8819300b943364b06a3dbf
Anton Imanov <antony.imanov@gmail.com>
2020-08-08 19:08:42 +0000

    Added process.env.Port

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use("/confs", confRoutes);
 app.use("/confs/:id/participants", participantRoutes);
 
 
-app.listen(3000, function(){
-	console.log('Server listening on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+	console.log('Server started');
 });
